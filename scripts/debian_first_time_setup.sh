b1526509f96af4fd1319e10f6126e4b6c82efe5e
Alexander-Mani <lexman02@gmail.com>
2026-05-18 18:46:07 +0000

    Fixed dual setup in setup script

--- scripts/debian_first_time_setup.sh
+++ scripts/debian_first_time_setup.sh
@@ -2,5 +2,3 @@
 python3.11 -m venv .venv && source .venv/bin/activate
 pip install --require-hashes --no-deps -r requirements/requirements.txt
 DEPLOY_PHASE=setup bash deployment.sh 
-DEPLOY_PHASE=smoke bash deployment.sh
-
